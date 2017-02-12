98e71b279ef7604ee9abf925053545323390747e
rkevinr <rkevinrnb@gmail.com>
2017-02-12 15:47:37 -0600

    baseline initial half page and Model obj done

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import cgi     # TODO:  remove if not used to escape stuff (vs. jinja2)
+import jinja2
+import os
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    blog_entry = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Handler(webapp2.RequestHandler):
+    """ A base RequestHandler class for our app.  """
+
+    def renderError(self, error_code):
+        """ Sends an HTTP error code + generic 'oops!' message to the client."""
+
+        self.error(error_code)
+        self.response.write("Oops! Something went wrong.")
+
+
+class SlashHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect('/blog')
+
+
+class NewBlogPostHandler(Handler):
+    def get(self):
+        # t = jinja_env.get_template("
+        self.response.write("get() for new blog post")
+
+temp_blogs_data = { 
+    'Blogs r Us': 'It was the best of times; it was the',
+    'Second Blog Post': 'I hear the third time\'s the charm' }
+
+class ViewBlogPostsHandler(Handler):
+    def get(self):
+        all_blogs = []
+        for blog_item in temp_blogs_data:
+            title = cgi.escape(blog_item)
+            blog_entry = cgi.escape(temp_blogs_data[blog_item])
+            b = BlogPost(title = title, blog_entry = blog_entry)
+            all_blogs.append(b)
+        t = jinja_env.get_template("bloghomepage.html")
+        content = t.render(blog_posts = all_blogs)
+        self.response.write(content)
+        
+
+        
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', SlashHandler),
+    ('/blog', ViewBlogPostsHandler),
+    ('/blog/newpost', NewBlogPostHandler)
 ], debug=True)
